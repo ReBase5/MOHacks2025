85574f03913a623e96d985750bb58280d53f3b0d
teamalexander5 <agpollaci@icloud.com>
2025-11-15 12:02:03 -0600

    forgot to stage from last commit

--- TideML.py
+++ TideML.py
@@ -3,9 +3,10 @@ import sklearn
 import numpy
 import pandas
 import matplotlib
+from TideData import IrishNationalTideGaugeNetwork
 
 class TideML():
-
+    tideData = IrishNationalTideGaugeNetwork
 
     def __init__ (self, tideData):
         self.tideData = tideData
