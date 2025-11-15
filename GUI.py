6f0f2421309ddab23b42533d41ec9a89b8fc9692
teamalexander5 <agpollaci@icloud.com>
2025-11-15 11:51:52 -0600

    GUI init

--- GUI.py
+++ GUI.py
@@ -3,7 +3,29 @@
 from tkinter import *
 
 window = Tk()
-window.geometry("420x420")
+window.geometry("512x512")
 window.title("OceanGPT")
+window.config(background = "#60b6d0")
+
+label = Label(window, 
+            text = "OceanGPT\nEnter a time to get the tide at that time:", 
+            font = ('Currior', 40, 'bold'), 
+            fg = '#88FF88', 
+            bg = '#60b6d0',
+            relief = RAISED,
+            bd = 10,
+            padx = 20,
+            pady = 20
+            )       
+label.pack()
+
+def click():
+    
+
+button = Button(window,
+                text = "Calculate",
+                command = click
+                )
+button.pack()
 
 window.mainloop()
